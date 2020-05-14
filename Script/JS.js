916cec058f31e5aad481a5029812a14fe973b425
KirandeepKaur03 <kkirandeep2@gmail.com>
2020-05-14 13:06:00 +0530

    done

--- Script/JS.js
+++ Script/JS.js
@@ -1,6 +1,6 @@
-let cont=document.getElementById("container4");
+let cont4=document.getElementById("container4");
 let backdrop=document.getElementById("backdrop");
-
+let cont7=document.getElementById("container7");
 
 function Toggle_Backdrop()
 {
@@ -9,10 +9,10 @@ function Toggle_Backdrop()
 
 function Toggle_Form()
 {
-  cont.classList.toggle("visible");
+  cont4.classList.toggle("visible");
   Toggle_Backdrop();
 };
-backdrop.addEventListener("click",Toggle_Form);
+
 
 function GenCaptcha(){
     let val = Math.floor(1000 + Math.random() * 9000);
@@ -20,3 +20,13 @@ function GenCaptcha(){
     let e1=document.getElementById('e1');        
 }
 
+function Toggle_Backdrop()
+{
+  backdrop.classList.toggle("visible");
+};
+
+function Toggle_LoginForm()
+{
+  cont7.classList.toggle("visible");
+  Toggle_Backdrop();
+};
